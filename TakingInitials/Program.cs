7b667175b358d8c1e7c12cb97b551f1ddab917db
Dharana Rodrigo <DharanaR@99x.io>
2021-01-21 16:53:33 +0530

    methods seperated

--- TakingInitials/Program.cs
+++ TakingInitials/Program.cs
@@ -6,44 +6,61 @@ namespace TakingInitials
     {
         static void Main(string[] args)
         {
-            //string to store the full name
-            string fullName;
+            string fullName = "";
+            convertName(fullName);
+        }
 
-            string name;
-            char initial;
-
+        public static void convertName(string fullName) 
+        {
             //readimg user input
             Console.Write("Enter full name with spaces: ");
             fullName = Console.ReadLine();
 
-            //check whether the name is empty
-            if(fullName != null && fullName != "")
+            if (fullName != null && fullName != "")
             {
-                //an array to store the words seperately
-                string[] nameByWords = fullName.Split();
-
-                Console.Write("Name with initials: ");
-
-                //taking the initials seperately
-                for (int i = 0; i < nameByWords.Length - 1; i++)
-                {
-                    name = nameByWords[i];
-
-                    initial = name[0];
-                    initial = Char.ToUpper(initial);
-
-                    //writing the initials
-                    Console.Write(initial + ". ");
-                }
-
-                //writing the last name
-                Console.WriteLine(nameByWords[nameByWords.Length - 1]);
-            } 
+                seperateInitials(fullName);
+            }
             else
             {
                 Console.WriteLine("Name cannot be empty!!!");
             }
+        }
 
+        public static void seperateInitials(string fullName)
+        {
+            string name;
+            char initial;
+            string nameWithInt;
+            string lastName;
+
+            //an array to store the words seperately
+            string[] nameByWords = fullName.Split();
+
+            Console.Write("Name with initials: ");
+
+            string initials = "";
+
+            //taking the initials seperately
+            for (int i = 0; i < nameByWords.Length - 1; i++)
+            {
+                name = nameByWords[i];
+
+                initial = name[0];
+                initial = Char.ToUpper(initial);
+
+                initials += initial.ToString() + ". ";
+            }
+
+            lastName = nameByWords[nameByWords.Length - 1];
+
+            nameWithInt = initials + lastName;
+
+            printNameWithInitials(nameWithInt);
+        }
+
+        public static void printNameWithInitials(string nameWithInt)
+        {
+            Console.WriteLine(nameWithInt);
         }
     }
 }
